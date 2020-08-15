bd2c3ab4d95e0b6806bf0c9dc2ee845dd7b5a09b
leonjhonn55 <69393802+leonjhonn55@users.noreply.github.com>
2020-08-15 12:03:04 -0500

    Clase 20200815

---
+++ SeleniumAutomation2/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,49 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/* 	
+		byte x;
+		short y;
+		int z;
+		long a;
+		
+		
+		x=127;
+		y=32767;
+		z=2147483647;
+		a=9223372036854775807L;
+		
+		System.out.println(x);
+*/
+		
+		/*   float base;
+		    double altura; 
+		    
+		    base= 30.50f;
+		    altura=40.76;
+		    
+		    System.out.println(altura);*/
+		
+		/*char letra1;
+		char letra2;
+		String frase;
+		
+		letra1='a';
+		letra2='x';
+		frase ="Hola";
+		
+		 System.out.println(frase);*/
+		
+		boolean avesvuelan;
+		
+		avesvuelan=true;
+		
+		System.out.println("Las aves vuelan "+avesvuelan);
+		
+	}
+		
+ 
+}
---
+++ SeleniumAutomation2/src/test/java/com/javabasic/OperatorsStatements.java
@@ -0,0 +1,48 @@
+package com.javabasic;
+
+public class OperatorsStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//aritmeticos	
+		
+	/*	int x=1; int y=2;
+		
+		int suma =(x+y);
+		
+		System.out.println(suma);*/
+		
+	//Operadores unarios
+		
+	/*	int valor1=3;
+		
+		int incrementar=++valor1;
+		
+		System.out.println(incrementar);*/
+		
+	/*	int a=3; int b=4;
+		
+		if(a==b){
+			System.out.println("Son iguales");
+			
+		}
+		else {
+			System.out.println("NO Son iguales");
+		}*/
+		
+//condicionesles
+		
+		int a=3; int b=4; int c=11;
+		if(a==c || a>c){
+			System.out.println("es menor que c");
+			
+		}
+		else {
+			System.out.println("NO Son iguales");
+		}
+		
+	}
+	
+
+}
